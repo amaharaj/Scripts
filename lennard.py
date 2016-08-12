aefb005541e9c35a585c22acc427cf1437ff7cd6
amaharaj <amber.maharaj@uoit.net>
2016-08-11 20:26:26 -0400

    Update lennard.py

--- lennard.py
+++ lennard.py
@@ -1,6 +1,11 @@
 import sys
 import numpy as np 
 
+# script to generate evenly spaced points over some range 
+# evaluated on a lennard jones potential 
+# generates LennardJones.txt 
+
+# execute as `python lennard.py <number of points to evaluate>`
 
 outfile = open('LennardJones.txt','w+')
 
@@ -11,11 +16,13 @@ def lennardJones(r, r_m, e):
    VLJ = e*((r_m/r)**12 - 2*(r_m/r)**6)
    return VLJ
 
+# generate distance values between some range
 r = np.linspace(1.0,2.0,divisions)
 
 VLJ = lennardJones(r,r_m=1.22,e=10)
 
 for i in range(len(r)): 
+   # print distance then potential evaluated at that distance to file
    outfile.write(str(r[i]) + " " + str(VLJ[i]) + "\n")
 
 outfile.close()
